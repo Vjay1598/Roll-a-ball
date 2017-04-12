bacbe260895e3d51d990270ef1faa3c1abe0cbb9
Vijayalakshmi Girijala <vijayalakshmi.girijala@gmail.com>
2017-04-12 21:50:04 +0530

    Added project

---
+++ demo.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class demo : MonoBehaviour {
+
+    public Light myLight;
+	
+	// Update is called once per frame
+	void Update () {
+	if (Input.GetKey("Space"))
+        {
+            myLight.enabled = true;
+        }
+    else
+        {
+            myLight.enabled = false;
+        }
+	}
+}
